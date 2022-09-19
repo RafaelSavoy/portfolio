aaeb9238daca6166efc33cb1a26d478c0cac4c2f
Rafael Savoy <rafaelsavoy1@outlook.com>
2022-09-19 11:57:10 -0300

    style(app): adicionando fontes

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html>
-            <Head />
+            <Head>
+                <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,regular,500,600,700,800,900,100italic,200italic,300italic,italic,500italic,600italic,700italic,800italic,900italic" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,regular,500,600,700,800,300italic,italic,500italic,600italic,700italic,800italic" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,regular,italic,500,500italic,700,700italic,900,900italic" rel="stylesheet" />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
