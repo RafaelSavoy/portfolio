cd44723fe58b6df506d675126d82ca2fd805b447
RafaelSavoy <89671665+RafaelSavoy@users.noreply.github.com>
2022-10-17 12:30:22 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-    console.log("Renderizou");
     return (
         <>
             <Head>
